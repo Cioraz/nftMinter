d06c68ea897a96472bd62ae130cb5749cf590bc4
Cioraz <123grover222@gmail.com>
2023-10-23 22:53:33 +0530

    Final edits

--- contracts/chocoGolemsNFT.sol
+++ contracts/chocoGolemsNFT.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.4;
-
+// Importing the ERC721 standards
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
